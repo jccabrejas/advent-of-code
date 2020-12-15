b3d3b6895ad3b8974846d436f8f1de9a1ae8b870
jcc <j.c.cabrejas@gmail.com>
2020-12-15 22:01:05 +0100

    2020-12-15 interim

--- solutions/2020/day_15/solution.py
+++ solutions/2020/day_15/solution.py
@@ -11,26 +11,29 @@ class Solution(BaseSolution):
 
     def part_1(self) -> int:
         sequence = [int(x) for x in self.input[0].split(',')]
+        last = sequence[-1]
+        sequence.pop(-1)
         seen = defaultdict(list)
 
         for idx, value in enumerate(sequence):
             seen[int(value)].append(idx+1)
 
         counter = len(sequence)
-        counter += 1
+        counter += 2
 
-        while counter <= 2020:
-            if (last := sequence[-1]) not in sequence[:-1]:
-                sequence.append(0)
+        while counter <= 30000000:
+            if last not in seen:
+                last = 0
                 seen[0].append(counter)
+                print(0)
             else:
                 diff = seen[last][-1] - seen[last][-2]
-                sequence.append(diff)
                 seen[diff].append(counter)
-
+                last = diff
+                print(last)
             counter +=1
 
-        return sequence[-1]
+        return last
 
     def part_2(self) -> int:
         pass
