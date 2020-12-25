781d931d640848c68fa3e1664c3f0c4f536bb66c
jcc <j.c.cabrejas@gmail.com>
2020-12-25 06:25:58 +0100

    day 24 part 2

--- solutions/2020/day_24/solution.py
+++ solutions/2020/day_24/solution.py
@@ -1,9 +1,9 @@
 from collections import defaultdict, Counter, deque
-# import numpy as np
+import numpy as np
 # import re
 import os, sys
 
-def read_input(filename='test.txt'):
+def read_input(filename='input.txt'):
     with open(os.path.join(os.getcwd(),filename)) as f:
         result = list()
         for line in f.readlines():
@@ -37,6 +37,31 @@ def parse_input(inputText):
 def move(x,y, delta, c):
     return x + c * delta[0], y + c * delta[1]
 
+def conway(lobby):
+    temp = lobby.copy()
+    rows, cols = lobby.shape
+
+    d ={True:1, False:0}
+
+    for r in range(2,rows-2):
+        for c in range(2,cols-2):
+            if (r+c)%2 != 0: continue
+            adjacents = 0
+            adjacents = d[lobby[r-1, c-1]] + d[lobby[r-1, c+1]] + \
+                          d[lobby[r, c-2]] + d[lobby[r, c + 2]] + \
+                          d[lobby[r+1, c-1]] + d[lobby[r+1, c+1]]
+            # Any black tile with zero or more than 2 black tiles immediately adjacent 
+            # to it is flipped to white.
+            if lobby[r,c]:
+                if adjacents == 0 or adjacents > 2:
+                    temp[r,c] = False
+            # Any white tile with exactly 2 black tiles
+            #  immediately adjacent to it is flipped to black.
+            else:
+                if adjacents == 2:
+                    temp[r,c] = True
+    return temp
+
 def part_1():
     tileDirections = parse_input(inputText)
     seen = defaultdict(int)
@@ -53,16 +78,34 @@ def part_1():
             x, y = move(x,y, transform[d], c)
         seen[(x,y)] += 1
     
-    print(seen)
+   
+    return seen
 
-    blacks = [tile for tile, color in seen.items() if color%2 == 1]
-    whites = [tile for tile, color in seen.items() if color%2 == 0]
+def part_2(blacks):
+    # xmax, ymax    (28, 14)
+    # xmin, ymin    (-25, -14)
+    size = 301
+    x0, y0 = size//2 + 1, size//2 + 1 
+    days = 100
+    lobby = np.array(size*size*[False]).reshape(size,size)
     
-    return len(blacks)
+    for b in blacks:
+        lobby[x0 + b[1], y0 + b[0]] = True
 
-def part_2():
-    pass
+    for day in range(days):
+        # print('Day ', day, ': ', sum(sum(lobby)))
+        lobby = conway(lobby).copy()
 
-print('\n=========================\nPart 1: ', part_1())
+    result = sum(sum(lobby))
+    print('Day ', day, ': ', result )
 
-print('\n=========================\nPart 2: ', part_2())
+    return result
+
+seen = part_1()
+blacks = [tile for tile, color in seen.items() if color%2 == 1]
+whites = [tile for tile, color in seen.items() if color%2 == 0]
+
+print('\n=========================\nPart 1: ', len(blacks))
+
+print('\n=========================\nPart 2: ')
+print(part_2(blacks))
